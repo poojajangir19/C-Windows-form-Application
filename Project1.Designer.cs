7499093a1ab7841b51c0a23c34180786287e1c40
poojajangir19 <50331805+poojajangir19@users.noreply.github.com>
2019-06-06 20:15:29 -0500

    Add files via upload

---
+++ Project1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Assign1
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clearButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.l = new System.Windows.Forms.Label();
+            this.t3 = new System.Windows.Forms.TextBox();
+            this.gb = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.RESULTS = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gb.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.Chocolate;
+            this.clearButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.clearButton.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clearButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.clearButton.Location = new System.Drawing.Point(74, 339);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(129, 55);
+            this.clearButton.TabIndex = 0;
+            this.clearButton.Text = "Close";
+            this.clearButton.UseMnemonic = false;
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.UseWaitCursor = true;
+            this.clearButton.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // okButton
+            // 
+            this.okButton.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.okButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.okButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.okButton.Location = new System.Drawing.Point(356, 154);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(106, 32);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "Execute";
+            this.okButton.UseMnemonic = false;
+            this.okButton.UseVisualStyleBackColor = false;
+            this.okButton.UseWaitCursor = true;
+            this.okButton.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // l
+            // 
+            this.l.AutoSize = true;
+            this.l.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l.Location = new System.Drawing.Point(25, 118);
+            this.l.Name = "l";
+            this.l.Size = new System.Drawing.Size(330, 24);
+            this.l.TabIndex = 2;
+            this.l.Text = "Type your SQL statement here";
+            this.l.UseWaitCursor = true;
+            // 
+            // t3
+            // 
+            this.t3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.t3.Location = new System.Drawing.Point(29, 159);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(278, 23);
+            this.t3.TabIndex = 7;
+            this.t3.UseWaitCursor = true;
+            // 
+            // gb
+            // 
+            this.gb.BackColor = System.Drawing.Color.Orange;
+            this.gb.Controls.Add(this.button1);
+            this.gb.Controls.Add(this.clearButton);
+            this.gb.Controls.Add(this.okButton);
+            this.gb.Controls.Add(this.t3);
+            this.gb.Controls.Add(this.l);
+            this.gb.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.gb.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gb.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb.Location = new System.Drawing.Point(0, 0);
+            this.gb.Name = "gb";
+            this.gb.Padding = new System.Windows.Forms.Padding(0, 1, 2, 3);
+            this.gb.Size = new System.Drawing.Size(540, 470);
+            this.gb.TabIndex = 9;
+            this.gb.TabStop = false;
+            this.gb.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Peru;
+            this.button1.Location = new System.Drawing.Point(281, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 55);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // RESULTS
+            // 
+            this.RESULTS.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RESULTS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.RESULTS.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.RESULTS.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.RESULTS.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RESULTS.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.RESULTS.FormattingEnabled = true;
+            this.RESULTS.HorizontalScrollbar = true;
+            this.RESULTS.ImeMode = System.Windows.Forms.ImeMode.AlphaFull;
+            this.RESULTS.ItemHeight = 17;
+            this.RESULTS.Location = new System.Drawing.Point(583, 69);
+            this.RESULTS.Margin = new System.Windows.Forms.Padding(5);
+            this.RESULTS.Name = "RESULTS";
+            this.RESULTS.ScrollAlwaysVisible = true;
+            this.RESULTS.Size = new System.Drawing.Size(479, 376);
+            this.RESULTS.TabIndex = 10;
+            this.RESULTS.UseTabStops = false;
+            this.RESULTS.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(733, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "DEPARTMENT DATA";
+            this.label1.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1076, 470);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RESULTS);
+            this.Controls.Add(this.gb);
+            this.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gb.ResumeLayout(false);
+            this.gb.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Label l;
+        private System.Windows.Forms.TextBox t3;
+        private System.Windows.Forms.GroupBox gb;
+        private System.Windows.Forms.ListBox RESULTS;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Project1.cs
@@ -0,0 +1,187 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Assign3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();   //to begin the application
+            Load += new EventHandler(Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //to connect to MySql and read files, process query and display results
+        {
+            String s = "server=10.158.56.53;uid=csci473g01;pwd=wordpass01;database=csci473g01;"; //source to connect to the database
+            MySqlConnection Con;     
+            using (Con = new MySqlConnection(s))
+            {
+                Con.Open(); MySqlCommand c;  //establishes new connection
+                string file, l, query;
+ //-----------------------------------------------------------------------------------------------------------------------------------------
+                file = "Room.txt"; l = ""; //first file is read
+                int column1, column2;
+                string[] entry; char position;
+                c = new MySqlCommand("drop table if exists RoomTable, OfficeTable, ClassTable;", Con); c.ExecuteNonQuery();  //creates new table if the same table doesn't exist
+                c = new MySqlCommand("create table ClassTable(Course integer,Section integer, Teacher char(20),Room integer,Time char(5),Days char(5),Enrollment integer);", Con); c.ExecuteNonQuery();
+                c = new MySqlCommand("create table RoomTable(Room integer,Capacity integer,Smart char(1));", Con); c.ExecuteNonQuery();
+                c = new MySqlCommand("create table OfficeTable(Teacher char(20),Office integer);", Con); c.ExecuteNonQuery();
+                    System.IO.StreamReader read = new System.IO.StreamReader(file); //object to read the input file
+                    l = read.ReadLine(); //every text is copied from the text file
+                    while ((System.IO.File.Exists(file)==true) && ((read.ReadLine()) != null)) //if the file exists and untill we reach the end of the file, the values are read
+                    {
+                        entry = l.Split(','); //since the values are split by ',' we are using this function
+                        string position1 = entry[0]; 
+                        string position2 = entry[1];
+                        position = entry[2][0];
+                        column1 = Convert.ToInt32(position1); column2 = Convert.ToInt32(position2);
+                        query = "insert into RoomTable values (" + column1 + ", " + column2 + ", '" + position + "');"; //values read are copied to the table
+                        c = new MySqlCommand(query, Con); c.ExecuteNonQuery(); //the command is passed and executed through the MySqlCommand object
+                    }
+                    read.Close(); //after reading text from the file, it is closed
+// -------------------------------------------------------------------------------------------------------------------------------------------
+                    file = "Office.txt"; //second file is read
+                    System.IO.StreamReader read1 = new System.IO.StreamReader(file); //Object used to read the file line by line
+                    string temp = read1.ReadLine();
+                    String columnn1; int columnn2;
+                    while (((System.IO.File.Exists(file)==true) && (read1.ReadLine()) != null))
+                    {
+                        entry = l.Split(',');
+                        string positionn;
+                        columnn1 = entry[0];
+                        positionn = entry[1];
+                        columnn2 = Convert.ToInt32(positionn); //the read value is converted to string
+                        query = "insert into OfficeTable values ('" + columnn1 + "', " + columnn2 + ");";
+                        c = new MySqlCommand(query, Con);  c.ExecuteNonQuery(); //Command is executed my establishing the connection
+                    }
+                    read1.Close();
+                //------------------------------------------------------------------------------------------------------------------------------------------
+                file = "Class.txt"; // reading the class file
+                System.IO.StreamReader read2 = new System.IO.StreamReader(file);
+                l = read2.ReadLine();
+                int len = (read2.ReadLine()).Length;
+                int colum1, colum2,colum4, colum7;
+                while (((l=read2.ReadLine()).Length>0) && (System.IO.File.Exists(file)==true))
+                {
+                    entry = l.Split(','); 
+                    string pos = entry[0]; //each entry is read and as per their datatype they are stored 
+                    string pos1 = entry[1];
+                    string pos2 = entry[2];
+                    string pos3 = entry[2];
+                    string pos4 = entry[3];
+                    string pos5 = entry[4];
+                    string pos6 = entry[5];
+                    string pos7 = entry[6];
+                    colum1 = Convert.ToInt32(pos);
+                    colum2 = Convert.ToInt32(pos1);
+                    colum7 = Convert.ToInt32(pos7);
+                if (pos4 == "null")  //since the fourth column has null values, we check for it
+                {
+                    colum4 = 0;
+                }
+                else
+                {
+                    colum4 = Convert.ToInt32(pos4);
+                }
+                query = "INSERT into ClassTable values (" + colum1 + ", " + colum2 + ",'" + pos3 + "'," + colum4 + ",'" + pos5 + "','" + pos6 + "'," + colum7 + ");";
+                c = new MySqlCommand(query, Con); c.ExecuteNonQuery();
+            }
+            read2.Close();
+                //------------------------------------------------------------------------------------------------------------------------
+                
+
+                DataTable t;
+                t = Con.GetSchema("Tables"); // The DB Schema is retrieved and tables are displayed in the listbox
+                string s1 = "pg";
+                string s2 = "sql";
+                foreach (DataRow row in t.Rows)
+                {
+                    string r = row[2].ToString();
+                    if (r.Substring(0, 2) != s1 && r.Substring(0, 3) != s2)
+                        RESULTS.Items.Add(r);
+                }
+
+            }
+        }
+        private void Ok_Click( object sender, EventArgs e)
+        {
+            RESULTS.Items.Add(" ");
+            string queryEntered, Conn;
+            MySqlConnection Con; MySqlCommand c;
+            queryEntered = t3.Text;
+            Conn = "server=10.158.56.53;uid=csci473g01;pwd=wordpass01;database=csci473g01;";
+            using (Con = new MySqlConnection(Conn))  //establishes connection 
+            {
+                Con.Open();   //opens the connection
+                MySqlDataReader d; int count;
+                string temp1, temp2;
+                temp1 = temp2 = "";
+                c = new MySqlCommand(queryEntered, Con);
+                try         //if the query entered doesn't exist, we need to throw an exceptio
+                {
+                    using ( d = c.ExecuteReader())
+                    {
+                        count = d.FieldCount; var v = d.GetSchemaTable();
+                        string Add; string b = " "; string value;
+                        foreach (DataRow row in v.Rows)
+                        {
+                            Add = row.Field<string>("ColumnName");
+                            int length = Add.Length;
+                            for (int i = length; i < 20; i++)
+                            {
+                                Add = Add + b;
+                            }
+                            temp2 = temp2 + (Add + "\t");
+                        }
+                        RESULTS.Items.Add(temp2);  //displays the values in the listbox
+                        while (d.Read()==true)
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                value = d[i].ToString();
+                                int len1 = value.Length;
+                                for (int j = len1; j < 20; j++)
+                                {
+                                    value = value + b;
+                                }
+
+                                temp1 = temp1 + (value + "\t");
+                            }
+                            RESULTS.Items.Add(temp1); temp1 = b;
+                        }
+
+                    }
+
+                }
+                catch (MySqlException)
+                {
+                    MessageBox.Show("Invalid query: Please enter a valid query");
+                }
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)  //clears the textbox
+        {
+            string clear = "";
+            t3.Text = clear;
+            RESULTS.Items.Clear();
+        }
+        private void cancel_Click(object sender, EventArgs e) //closes the application
+        {
+            Application.Exit();
+        }
+    }
+}
+
